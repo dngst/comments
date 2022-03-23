70b75628558ac2f24180e7bcaf38c69ead613e54
dngst <dngst@protonmail.com>
2022-03-23 22:00:21 +0300

    Highlight error message

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,15 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @comment = Comment.create!(comment_params)
+    @comment = Comment.new(comment_params)
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to root_path }
+      else
+        format.html { redirect_to root_path, notice: @comment.errors.full_messages }
+      end
+    end
   end
 
   private
