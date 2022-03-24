9153111efcac2a46d60a4adcf8950eb7fffa1bc7
dngst <dngst@protonmail.com>
2022-03-24 18:15:34 +0300

    Add test for valid/invalid submission

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -5,4 +5,29 @@ describe 'the comments page', type: :feature do
     visit '/'
     expect(page).to have_content 'comments'
   end
+
+  context 'when submission is valid' do
+    it 'adds a comment' do
+      visit '/'
+      fill_in 'body', with: 'turbo'
+      click_button 'Save'
+      expect(page).to have_content 'turbo'
+    end
+
+    it 'shows a comment in lowercase' do
+      visit '/'
+      fill_in 'body', with: 'STAY'
+      click_button 'Save'
+      expect(page).to have_content 'stay'
+    end
+  end
+
+  context 'when submission is invalid' do
+    it 'shows error message' do
+      visit '/'
+      fill_in 'body', with: ''
+      click_button 'Save'
+      expect(page).to have_content 'Body can\'t be blank'
+    end
+  end
 end
