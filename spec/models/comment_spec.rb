55e9a981f2801171156deeefea6f948922ace57e
dngst <dngst@protonmail.com>
2022-03-26 21:35:40 +0300

    Test lowercase pre-create callback

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
+  let!(:comment) { create(:comment) }
+
   it { is_expected.to validate_presence_of :body }
+
+  it 'lower cases the body' do
+    expect(comment.body).to eq(comment.body.downcase)
+  end
 end
